e2d46d3310414ff460de28140c80777c9a75267a
MclPio <113801014+MclPio@users.noreply.github.com>
2024-01-17 14:32:13 -0500

    Implemented sign in with google

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  
+  root 'pages#home'
+  devise_for :users, controllers: {
+    registerations: 'user/registrations',
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
 end
