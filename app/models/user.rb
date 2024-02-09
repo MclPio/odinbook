11a846968c472a97512f6166e79185b072ace8b2
MclPio <113801014+MclPio@users.noreply.github.com>
2024-02-09 12:25:38 -0500

    Initial comments scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,6 @@ class User < ApplicationRecord
   
   has_many :likes
   has_many :liked_posts, through: :likes, source: :post
+
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_06_224922) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_09_171156) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_comments_on_user_id_and_post_id", unique: true
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "follows", force: :cascade do |t|
     t.integer "follower_id"
@@ -60,6 +71,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_06_224922) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
