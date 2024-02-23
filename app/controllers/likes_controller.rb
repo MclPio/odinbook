fae1cb2e5406325f1ea1923ecd4dcd9291180384
MclPio <113801014+MclPio@users.noreply.github.com>
2024-02-22 20:18:10 -0500

    Save progress

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,19 +3,17 @@ class LikesController < ApplicationController
 
   def create
     @like = current_user.likes.new(like_params)
-
     if @like.save
       flash[:notice] = @like.errors.full_messages.to_sentence
     end
 
-    redirect_to @like.post
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @like = current_user.likes.find(params[:id])
-    post = @like.post
     @like.destroy
-    redirect_to post
+    redirect_back(fallback_location: root_path)
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,5 @@ class Post < ApplicationRecord
   has_many :liking_users, through: :likes, source: :user
   has_many :comments
 
-  validates :body, presence: true
+  validates :body, presence: true, length: { maximum: 280 }
 end
