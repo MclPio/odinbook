63a5e50d4ccf82164c8af866f9e2b8f28d706571
MclPio <113801014+MclPio@users.noreply.github.com>
2024-02-26 20:39:19 -0500

    Implement follow in user/show

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -13,9 +13,9 @@ class FollowsController < ApplicationController
     @follow = Follow.new(follow_params)
 
     if @follow.save
-      redirect_to users_path
+      redirect_back fallback_location: root_path
     else
-      redirect_to users_path, status: :unprocessable_entity
+      redirect_back fallback_location: root_path, status: :unprocessable_entity
     end
   end
 
