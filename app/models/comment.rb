e91ae3b413cca18e2f8fdbadd1ed7cab2d4105ce
MclPio <113801014+MclPio@users.noreply.github.com>
2024-03-28 20:30:28 -0400

    add word length limit to post and comment models, fix delete bug for posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,9 @@ class Comment < ApplicationRecord
   belongs_to :parent, class_name: 'Comment', optional: true
   has_many :comments, foreign_key: :parent_id, dependent: :destroy
 
-  validates :body, presence: true, length: { maximum: 280 }
+  validates :body, presence: true, length: { maximum: 280 }, 
+            format: { with: /\b\w{1,50}\b/,
+            message: "cannot contain more than 50 consecutive non-space characters" }
+
   validates :depth, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,7 @@ class Post < ApplicationRecord
   has_many :poly_likes, as: :likable
   has_many :comments, dependent: :destroy
 
-  validates :body, presence: true, length: { maximum: 280 }
+  validates :body, presence: true, length: { maximum: 280 },
+            format: { with: /\b\w{1,50}\b/, 
+                      message: "cannot contain more than 50 consecutive non-space characters" }
 end
